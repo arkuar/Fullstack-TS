96bc741b3361d29ac62f947ecdd2397cc1c1c18c
arkuar <arttukuronen@gmail.com>
2021-02-16 19:25:21 +0200

    9.6 - 9.7

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,7 +1,7 @@
-type Rating = 1 | 2 | 3
+type Rating = 1 | 2 | 3;
 
-type RatingDescription = "ok" | "not too bad but could be better"
-  | "great!"
+type RatingDescription = "bad" | "not too bad but could be better"
+  | "great!";
 
 interface ExerciseResult {
   periodLength: number;
@@ -14,28 +14,28 @@ interface ExerciseResult {
 }
 
 function calculateRating(ratio: number): { rating: Rating, ratingDescription: RatingDescription } {
-  let rating: Rating
-  let ratingDescription: RatingDescription
+  let rating: Rating;
+  let ratingDescription: RatingDescription;
   if (ratio < 0.4) {
-    rating = 1
-    ratingDescription = "ok"
+    rating = 1;
+    ratingDescription = "bad";
   } else if (ratio < 0.9) {
-    rating = 2
-    ratingDescription = "not too bad but could be better"
+    rating = 2;
+    ratingDescription = "not too bad but could be better";
   } else {
-    rating = 3
-    ratingDescription = "great!"
+    rating = 3;
+    ratingDescription = "great!";
   }
   return {
     rating, ratingDescription
-  }
+  };
 }
 
-function calculateExercises(hours: Array<number>, target: number): ExerciseResult {
-  const trainingDays = hours.filter(d => d > 0).length
-  const overTarget = hours.filter(d => d >= target).length
-  const success = overTarget === hours.length
-  const { rating, ratingDescription } = calculateRating(overTarget / trainingDays)
+export function calculateExercises(hours: Array<number>, target: number): ExerciseResult {
+  const trainingDays = hours.filter(d => d > 0).length;
+  const overTarget = hours.filter(d => d >= target).length;
+  const success = overTarget === hours.length;
+  const { rating, ratingDescription } = calculateRating(overTarget / hours.length);
   
   return {
     periodLength: hours.length,
@@ -45,22 +45,22 @@ function calculateExercises(hours: Array<number>, target: number): ExerciseResul
     ratingDescription,
     target,
     average: hours.reduce((sum, val) => sum + val) / hours.length
-  }
+  };
 }
 
 function parseExerciseArgs(args: Array<string>): { target: number, hours: Array<number> } {
-  if (args.length < 2) throw new Error("Not enough arguments")
-  if (args.some(v => isNaN(Number(v)))) throw new Error("Provided values were not numbers")
+  if (args.length < 2) throw new Error("Not enough arguments");
+  if (args.some(v => isNaN(Number(v)))) throw new Error("Provided values were not numbers");
 
   return {
     target: Number(args[0]),
     hours: args.slice(1).map(v => Number(v))
-  }
+  };
 }
 
 try {
-  const { target, hours } = parseExerciseArgs(process.argv.slice(2))
-  console.log(calculateExercises(hours, target))
+  const { target, hours } = parseExerciseArgs(process.argv.slice(2));
+  console.log(calculateExercises(hours, target));
 } catch (e) {
-  console.log(`Error encountered. Message: ${e.message}`)
+  console.log(e);
 }
--- index.ts
+++ index.ts
@@ -1,23 +1,52 @@
-import express from 'express';
+import bodyParser from 'body-parser';
+import express, { Request } from 'express';
+
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
+
 const app = express();
+app.use(bodyParser.json()); // for parsing application/json
+
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
 app.get('/bmi', (req, res) => {
-  const height = Number(req.query.height)
-  const weight = Number(req.query.weight)
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
   if (isNaN(height) || isNaN(weight)) {
-    res.status(400).send({ error: "malformatted parameters" })
+    res.status(400).send({ error: "malformatted parameters" });
   } else {
-    const bmi = calculateBmi(height, weight)
+    const bmi = calculateBmi(height, weight);
     res.send({
       weight, height, bmi
-    })
+    });
   }
-})
+});
+
+interface CustomRequest extends Request {
+  body: {
+    daily_exercises: Array<number>,
+    target: number
+  }
+}
+
+app.post('/exercises', (req: CustomRequest, res) => {
+  const { daily_exercises, target } = req.body;
+  if (!daily_exercises || target === undefined) {
+    res.status(400).send({
+      error: "parameters missing"
+    });
+  } else if (daily_exercises.some(v => isNaN(v)) || isNaN(target)) {
+    res.status(400).send({
+      error: "malformatted parameters"
+    });
+  } else {
+    const result = calculateExercises(daily_exercises, target);
+    res.send(result);
+  }
+});
 
 const PORT = 3002;
 
